f955d8800c5265c865ebb8053d3152012dbd59e4
SillyFunc <erickwok@tuta.io>
2026-09-16 14:09:53 +0800

    chore: remove unused useBoardList hook from MainLayout

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -2,12 +2,9 @@ import { useState } from 'react'
 import { Outlet } from 'react-router'
 import { Header } from '@/components/common/header'
 import { Sidebar } from '@/components/common/sidebar'
-import { useBoardList } from '@/hooks/use-board'
 
 export const MainLayout = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
-
-  useBoardList({ model: 1 })
 
   return (
     <div className="relative size-full bg-black flex min-h-dvh flex-col">
