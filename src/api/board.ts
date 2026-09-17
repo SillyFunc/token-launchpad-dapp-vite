778c041bce1ac68cc4a8bdb65b76880b4b746ce8
SillyFunc <erickwok@tuta.io>
2026-09-17 14:53:58 +0800

    feat: expand BoardItemResponse type with detailed properties for better data handling

--- src/api/board.ts
+++ src/api/board.ts
@@ -8,7 +8,100 @@ export interface BoardListParams extends PageParams {
   canSwap?: 0 | 1
 }
 
-export type BoardItemResponse = Record<string, unknown>
+export type BoardItemResponse = {
+  id: number
+  hash: string
+  name: string
+  address: string
+  creatorAddress: string
+  contractAddress: string
+  coinContractAddress: string
+  chatAt: string
+  unit: number
+  minTxFee: number
+  maxTxFee: number
+  marketCap: number
+  feeLogicType: number
+  secretAddress: string
+  fullName: string
+  coinPrecision: string
+  coinImg: string
+  issuePrice: number
+  totalIssuance: number
+  calculationRate: number
+  issuanceCycle: number
+  issuer: string
+  officialWebsite: string
+  whitePaperLink: string
+  contractInformation: string
+  releaseDeclaration: string
+  minHoldBalance: number
+  status: number
+  coinStatus: number
+  auditRemark: string
+  auditTime: string
+  createTime: string
+  realPrice: number
+  presalePrice: number
+  feeRecipient: string
+  network: string
+  tradePrice: number
+  totalSupply: number
+  maxTotalNum: number
+  softCapRate: number
+  presaleMaxNum: number
+  insideMaxNum: number
+  backingReceiver: string
+  startTime: string
+  endTime: string
+  lgeCopies: number
+  rate: number
+  vestingDis: number
+  hardcap: number
+  softcap: number
+  tokenAmount: number
+  maxBuyPerWallet: number
+  vestingDelay: number
+  vestingRate: number
+  backingShare: number
+  userLpShare: number
+  devLpShare: number
+  devLpReceiver: string
+  basePresaleCount: number
+  website: string
+  telegram: string
+  twitter: string
+  withdrawFeeRate: number
+  pledgeContractAddress: string
+  minWithdrawAmount: number
+  maxWithdrawAmount: number
+  zhIntroduction: string
+  enIntroduction: string
+  thb: string
+  soft: number
+  remainSupply: string
+  domesticPrice: number
+  presaleAddress: string
+  top: number
+  isInternalExchange: number
+  launchType: number
+  canSwap: number
+  tradeAddress: string
+  backingPoolAddress: string
+  symbol: string
+  meta: string
+  buyTax: number
+  sellTax: number
+  taxDuration: number
+  antiFarmerDuration: number
+  liqExpectedOutputAmount: string
+  salt: string
+  creationFee: string
+  presaleTokenPrice: number
+  minLiquidityAmount: number
+  slippage: number
+  creatorBuyTokens: number
+}
 
 export function listBoard(params: BoardListParams, signal?: AbortSignal) {
   return postForm<PageResult<BoardItemResponse>>(
--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,5 +1,12 @@
+import { useBoardList } from '@/hooks/use-board'
 import { m } from '@/paraglide/messages.js'
+import {  useConnection } from 'wagmi'
 
 export const DashboardPage = () => {
+  const {address} = useConnection()
+  useBoardList({
+    model: 0,
+    address,
+  })
   return <div>{m.dashboard_page()}</div>
 }
